0ed49fb568463556426b0d4a787614e43d5d8524
Estudiante <Estudiante@DESKTOP-2AIF6UK>
2023-05-24 10:58:31 -0600

    Con Login Y registro

---
+++ ActivityRegistro.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Widget;
+
+namespace AgendaOnline
+{
+    [Activity(Label = "ActivityRegistro")]
+    public class ActivityRegistro : Activity
+    {
+        EditText Nombre, Apellido, Telf, Email, Pass, RePass;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.registro);
+            //Validamos las contraseñas
+            Pass = FindViewById<EditText>(Resource.Id.editText4);
+            RePass = FindViewById<EditText>(Resource.Id.editText6);
+
+            if(Pass.Text==RePass.Text)
+            {
+                //Vinculamos las instancias de variables con los controles
+                Nombre= FindViewById<EditText>(Resource.Id.editText1);
+                Apellido = FindViewById<EditText>(Resource.Id.editText2);
+                Email = FindViewById<EditText>(Resource.Id.editText3);
+                Telf = FindViewById<EditText>(Resource.Id.editText5);
+
+                //Invocamos el metodo para registrar
+               // ClienteWS C = new ClienteWS();//Con instanciamiento de la clase
+                
+               if(ClienteWS.CrearUsuario(Nombre.Text, Apellido.Text,Telf.Text,Email.Text,Pass.Text))
+                {
+                    Toast.MakeText(this, "Se ha registrado con Exito", ToastLength.Long).Show();
+                    ClienteWS.Email = Email.Text;
+                    ClienteWS.Pass = Pass.Text;
+                }
+               else
+                {
+                    Toast.MakeText(this, "No se realizo el registro", ToastLength.Long).Show();
+                }
+
+            }
+            else
+            {
+                Toast.MakeText(this, "Contraseñas no coinciden, vuela a intentar ", ToastLength.Long).Show();
+            }
+        }
+    }
+}
---
+++ ClienteWS.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgendaOnline.com.somee.pogramaweb2023;
+
+namespace AgendaOnline
+{
+    /*
+     Dentro de esta clase crearemos 
+    cada uno de los metodos correspondientes a los respectivos 
+    webmethod del servicio web
+     */
+    class ClienteWS
+    {
+        //Almacenar las credenciales
+        public static string Email = "";
+        public static string Pass = "";
+        //Instanciar el servicio web
+        public static com.somee.pogramaweb2023.AgendaOnlineWS Servicio = new com.somee.pogramaweb2023.AgendaOnlineWS();
+
+        //Definimos los metetodos que funcionaran como interfaces
+
+        public static bool CrearUsuario(string Nombre, string Apellido, string Telf, string Email, string Pass)
+        {
+            return Servicio.CreateUser(Nombre, Apellido, Email, Telf, Pass);
+        }
+
+        public int IniciarSesion(string Email, string Pass)
+        {
+            return Servicio.Logueo(Email, Pass);
+        }
+
+        public bool CrearGrupo(string Email, string Pass, string NombreGrupo, string Color)
+        {
+            return Servicio.CrearGrupo(Email, Pass, NombreGrupo, Color);
+        }
+
+        public bool CrearContacto(string Email, string Pass, int idGrupo, ContactoWS Contacto)
+        {
+            return Servicio.CrearContacto(Email, Pass, idGrupo, Contacto);
+        }
+
+        public List<GrupoWS> ListarGrupos(string Email, string Pass)
+        {
+            return Servicio.ListarGrupos(Email, Pass).ToList();
+        }
+
+        public List<ContactoWS> ListarContactos(string Email, string Pass, int idGrupo)
+        {
+            return Servicio.ListarContactos(Email, Pass, idGrupo).ToList();
+        }
+    }
+}
